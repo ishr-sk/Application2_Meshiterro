bf537739051365d3e47bbb12678673fc171d55fd
ishr-sk <kmrttnr1208@gmail.com>
2021-06-13 17:01:23 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '12bd23892f28815c6d1f928bef83ba6791903836b8b51508ee1d89bb32c471cf68b18671eb84e9672ecfb8ce94cdeaba7e95b3fa0253bae517b3a92d49619027'
